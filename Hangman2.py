f5e33e518963e3f194b6569be8d9ee965977f79f
Shrey Suri <shreysuri5@gmail.com>
2022-04-04 19:52:16 -0500

    Wrote the starting code - UI

---
+++ Hangman2.py
@@ -0,0 +1,20 @@
+print("Welcome to Hangman. The rules of this game can be found in the README file. Note that if a word contains the same letter multiple types, you need to guess that letter multiple times.")
+print("")
+
+a = input(print("If you would like a hint, type 'list' to pull up the word list."))
+a = str(a)
+a = a.upper()
+if a == "LIST":
+    print("Units are categorized by what they quantify")
+    print("")
+    print("Distance          Volume        Mass")
+    print("")
+    print("ounce             inch         ounce")
+    print("cup               foot         pound")
+    print("pint              yard         ton")
+    print("quart             mile")
+    print("gallon")
+    print("")
+    print("Hope this helps")
+else:
+    print("I didn't understand. No helpful hints for you!")
