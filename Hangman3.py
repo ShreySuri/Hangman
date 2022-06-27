30ab94d71340f4e319af8361c4b361359fb7737c
Shrey Suri <shreysuri5@gmail.com>
2022-06-26 19:08:39 -0500

    almost done

--- Hangman3.py
+++ Hangman3.py
@@ -133,19 +133,19 @@ for i in range (0, letters):
     string = "_ "
     known_word.append(string)
 incorrect = 0
+correct_count = 0
 
+print(listToString(known_word))
 
-while game == True:
-    counter = 0
-    while counter != 1:
+while game == True:    
+    valid_guess = False
+    while valid_guess == False:
         guess = input(print("Please guess a letter. If you would like a word library, type 'library'."))
         guess = guess.lower()
-        print(guess)
 
         for i in range (0, 26):
             if all_letters[i] == guess:
-                counter = 1
-                print(all_letters[i])
+                valid_guess = True
             else:
                 toggle = True
                 
@@ -181,13 +181,110 @@ while game == True:
 
     if correct == True:
         str_known_word = listToString(known_word)
-        print(str_known_word)
+        full_str = "Word: %s" % str_known_word
+        print(full_str)
+        print("")
+        correct_count = correct_count + 1
+        if correct_count == letters:
+            game = False
+            win = True
+        else:
+            toggle = True
     else:
+        string = "%s " % guess
         guesses.append(string)
         str_guesses = listToString(guesses)
-        print(str_guesses)
+        full_str = "Incorrect Guesses: %s" % str_guesses
+        print(full_str)
+        print("")
         incorrect = incorrect + 1
-    print("")
+
+        if incorrect == 1:
+            t_gallow.width(3)
+            t_gallow.pencolor("tan")
+            t_gallow.forward(100)
+        elif incorrect == 2:
+            t_gallow.width(1)
+            t_gallow.pencolor("black")
+            t_gallow.up()
+            t_gallow.right(90)
+            t_gallow.down()
+            t_gallow.circle(50)
+            t_gallow.left(90)
+            t_gallow.up()
+            t_gallow.forward(100)
+        elif incorrect == 3:
+            t_gallow.down()
+            t_gallow.forward(250)
+        elif incorrect == 4:
+            t_gallow.right(180)
+            t_gallow.forward(200)
+            t_gallow.left(150)
+            t_gallow.forward(150)
+        elif incorrect == 5:
+            t_gallow.right(180)
+            t_gallow.forward(150)
+            t_gallow.right(120)
+            t_gallow.forward(150)
+        elif incorrect == 6:
+            t_gallow.right(180)
+            t_gallow.forward(150)
+            t_gallow.left(150)
+            t_gallow.forward(200)
+            t_gallow.right(30)
+            t_gallow.forward(150)
+        elif incorrect == 7:
+            t_gallow.right(180)
+            t_gallow.forward(150)
+            t_gallow.right(120)
+            t_gallow.forward(150)
+        elif incorrect == 8:
+            t_gallow.right(180)
+            t_gallow.forward(150)
+            t_gallow.right(30)
+            t_gallow.up()
+            t_gallow.forward(325)
+            t_gallow.left(90)
+            t_gallow.forward(25)
+            t_gallow.left(135)
+            t_gallow.down()
+            t_gallow.forward(28.28)
+            t_gallow.up()
+            t_gallow.left(135)
+            t_gallow.forward(20)
+            t_gallow.left(135)
+            t_gallow.down()
+            t_gallow.forward(28.28)
+            t_gallow.up()
+            t_gallow.right(135)
+            t_gallow.forward(20)
+            t_gallow.right(90)
+            t_gallow.forward(30)
+            t_gallow.right(180)
+            t_gallow.left(135)
+            t_gallow.down()
+            t_gallow.forward(28.28)
+            t_gallow.up()
+            t_gallow.left(135)
+            t_gallow.forward(20)
+            t_gallow.left(135)
+            t_gallow.down()
+            t_gallow.forward(28.28)
+            t_gallow.up()
+            t_gallow.left(45)
+            t_gallow.forward(25)
+            t_gallow.left(90)
+            t_gallow.forward(30)
+            t_gallow.right(180)
+            t_gallow.down()
+            t_gallow.forward(70)
+
+            game = False
+            win = False
+            
+        else:
+            print("Something went wrong.")
+            
         
         
                
